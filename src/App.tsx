ea43e310f6de07a53e3c99aa1551ef4c462b5fdb
GhazaryanYuri <yuranar12@gmail.com>
2025-08-10 15:47:32 +0400

    Added React router and React router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 // Importing dependencies
+import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/01-Header/Header";
+import Home from "./pages/01-Home/Home";
 
 const App = () => {
   return (
     <div className="w-full h-auto flex-col justify-center items-center">
       <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 };
--- src/components/02-Navber/Navbar.tsx
+++ src/components/02-Navber/Navbar.tsx
@@ -1,5 +1,11 @@
 // Importing dependecies
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+// Importing icons
+import CartIcon from "../../assets/icons/cart.svg";
+import HearthIcon from "../../assets/icons/hearth.svg";
+import UserIcon from "../../assets/icons/user.svg";
 
 interface NavbarLinks {
   text: string;
@@ -8,6 +14,21 @@ interface NavbarLinks {
 
 const Navbar = () => {
   const [navbarLinks, setNavbarLinks] = useState<NavbarLinks[]>([]);
+  const iconsArray = [
+    {
+      icon: CartIcon,
+      url: "#",
+    },
+    {
+      icon: HearthIcon,
+      url: "#",
+    },
+    {
+      icon: UserIcon,
+      url: "#",
+    },
+  ];
+  const [activeLink, setActiveLink] = useState<number>(0);
 
   async function fetchingNavbarData() {
     try {
@@ -27,14 +48,40 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div>
-      <input type="search" placeholder="Search product..." />
+    <div className="w-full h-auto flex justify-around items-center mt-8">
+      <input
+        type="search"
+        placeholder="Search product..."
+        className="border-2 p-1 rounded-[6px] border-[#0A0A0A] focus:outline-none"
+      />
 
-      <ul>
+      <ul className="flex gap-12.5">
         {navbarLinks &&
           navbarLinks.map((link, index) => (
             <li key={index}>
-              <a href={link.url}>{link.text}</a>
+              <Link
+                to={link.url}
+                className={`cursor-pointer text-base font-bold transition-all hover:text-[#979797] ${
+                  activeLink === index ? "text-[#979797]" : "text-[#0A0A0A]"
+                }`}
+                onClick={() => setActiveLink(index)}
+              >
+                {link.text}
+              </Link>
+            </li>
+          ))}
+      </ul>
+      <ul className="flex gap-6">
+        {iconsArray &&
+          iconsArray.map((icon, index) => (
+            <li key={index}>
+              <a href={icon.url}>
+                <img
+                  src={icon.icon}
+                  alt={icon.url}
+                  className="hover:scale-125 transition-all"
+                />
+              </a>
             </li>
           ))}
       </ul>
